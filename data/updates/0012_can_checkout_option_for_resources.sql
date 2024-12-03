0f4512dfe3054c2408f611ceac86abbbccdc1714
jburgher2 <jburgher2@DESKTOP-ROKKR3G>
2024-12-02 18:06:08 -0600

    added tables 'checkout_tools' and 'checkout_log' for tools that can be checked out at the kiosk.

--- data/updates/0012_can_checkout_option_for_resources.sql
+++ data/updates/0012_can_checkout_option_for_resources.sql
@@ -1,1 +1,19 @@
-ALTER TABLE `resources` ADD COLUMN `can_checkout` tinyint(1) DEFAULT 0;
+CREATE TABLE `checkout_tools` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `is_checked_out` tinyint(1) DEFAULT 0,
+  `barcode` varchar(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+
+CREATE TABLE `checkout_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `tool_id` int(11) NOT NULL,
+  `checkout_date` datetime DEFAULT NULL,
+  `return_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  FOREIGN KEY (`tool_id`) REFERENCES `checkout_tools` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
